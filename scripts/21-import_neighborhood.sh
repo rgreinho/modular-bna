3a5b297adcd10b74c0de25261310a0033dda59a2
Rémy Greinhofer <remy.greinhofer@gmail.com>
2023-07-09 16:39:35 -0500

    Improve import neighborhood script
    Cleans up the script import the neighborhood data.
    
    Signed-off-by: Rémy Greinhofer <remy.greinhofer@gmail.com>

--- scripts/21-import_neighborhood.sh
+++ scripts/21-import_neighborhood.sh
@@ -3,30 +3,17 @@ set -euo pipefail
 [ "${PFB_DEBUG}" -eq "1" ] && set -x
 
 GIT_ROOT=$(git rev-parse --show-toplevel)
-# NB_TEMPDIR="test/data"
+IMPORT_SRID=4326
 NB_MAX_TRIP_DISTANCE=2680
 NB_BOUNDARY_BUFFER=$NB_MAX_TRIP_DISTANCE
-
-# Function to import a shapefile (using 'dump' mode for quickness) and convert it to the target SRID
-function import_and_transform_shapefile() {
-  IMPORT_FILE="${1}"
-  IMPORT_TABLENAME="${2}"
-  IMPORT_SRID="${3:-4326}"
-
-  echo "START: Importing ${IMPORT_TABLENAME}"
-  # https://manpages.debian.org/stretch/postgis/shp2pgsql.1.en.html
-  shp2pgsql -c -I -D -s "${IMPORT_SRID}" "${IMPORT_FILE}" "${IMPORT_TABLENAME}" | psql
-  # shp2pgsql -I -d -D -s "${IMPORT_SRID}" "${IMPORT_FILE}" "${IMPORT_TABLENAME}" | psql
-  psql -c "ALTER TABLE ${IMPORT_TABLENAME} ALTER COLUMN geom \
-            TYPE geometry(MultiPolygon,${NB_OUTPUT_SRID}) USING ST_Force2d(ST_Transform(geom,${NB_OUTPUT_SRID}));"
-  # remyg: do we need the alter command if we specify the output SRID in the shp2pgsql command? -s"${IMPORT_SRID}:${NB_OUTPUT_SRID}"
-  echo "DONE: Importing ${IMPORT_TABLENAME}"
-}
-
-# Import neighborhood boundary
-import_and_transform_shapefile "${NB_BOUNDARY_FILE}" neighborhood_boundary "${NB_INPUT_SRID}"
-
 NB_COUNTRY=$(echo "$NB_COUNTRY" | tr '[:lower:]' '[:upper:]')
+SHP2PGSQL_CMD="shp2pgsql -d -I -D -s ${IMPORT_SRID}:${NB_OUTPUT_SRID}"
+
+# Import neighborhood boundary.
+echo "IMPORTING: Loading neighborhood boundary: ${NB_BOUNDARY_FILE}"
+${SHP2PGSQL_CMD} "${NB_BOUNDARY_FILE}" neighborhood_boundary | psql
+
+# Import waterblocks.
 if [ "${NB_COUNTRY}" == "USA" ]; then
   echo "IMPORTING: Downloading water blocks"
   psql <"${GIT_ROOT}/sql/create_us_water_blocks_table.sql"
@@ -36,28 +23,31 @@ fi
 
 # Import block shapefile
 echo "IMPORTING: Loading census blocks"
-import_and_transform_shapefile "${NB_TEMPDIR}/population.shp" neighborhood_census_blocks 4326
+${SHP2PGSQL_CMD} "${NB_TEMPDIR}/population.shp" neighborhood_census_blocks | psql
 
 # Only keep blocks in boundary+buffer
-echo "IMPORTING: Applying boundary buffer"
 echo "START: Removing blocks outside buffer with size ${NB_BOUNDARY_BUFFER}"
-psql -c "DELETE FROM neighborhood_census_blocks AS blocks USING neighborhood_boundary \
-        AS boundary WHERE NOT ST_DWithin(blocks.geom, boundary.geom, \
-        ${NB_BOUNDARY_BUFFER});"
+psql <<SQL
+DELETE FROM neighborhood_census_blocks AS blocks
+USING neighborhood_boundary AS boundary
+WHERE NOT ST_DWITHIN(blocks.geom, boundary.geom, ${NB_BOUNDARY_BUFFER});
+SQL
 echo "DONE: Finished removing blocks outside buffer"
 
+# Discard blocks that are all water / no land area
 if [ "${NB_COUNTRY}" == "USA" ]; then
-  # Discard blocks that are all water / no land area
-  echo "IMPORTING: Removing water blocks"
   echo "START: Removing blocks that are 100% water from analysis"
-  psql -c "DELETE FROM neighborhood_census_blocks AS blocks USING water_blocks \
-           AS water WHERE blocks.BLOCKID10 = water.geoid;"
+  psql <<SQL
+DELETE FROM neighborhood_census_blocks AS blocks
+USING water_blocks AS water
+WHERE blocks.blockid10 = water.geoid;
+SQL
   echo "DONE: FINISHED removing blocks that are 100% water"
 fi
 
 POPULATION=$(psql -t -c "SELECT SUM(pop10) FROM neighborhood_census_blocks;")
 echo "Population is ${POPULATION}"
-if [[ $POPULATION -eq 0 ]]; then
+if [ "$POPULATION" -eq 0 ]; then
   echo "Error: Total population of included census blocks is zero."
   exit 1
 fi
