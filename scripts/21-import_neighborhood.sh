deffa68ac0a30dc141364157fd5704a95004d811
Rémy Greinhofer <remy.greinhofer@gmail.com>
2023-07-16 21:02:41 -0500

    Wire UTM detection script (#28)
    Wire the UTM detection script to detect the output SRID base on the
    boundary file.

--- scripts/21-import_neighborhood.sh
+++ scripts/21-import_neighborhood.sh
@@ -3,7 +3,6 @@ set -euo pipefail
 [ "${PFB_DEBUG}" -eq "1" ] && set -x
 
 GIT_ROOT=$(git rev-parse --show-toplevel)
-# NB_TEMPDIR="test/data"
 NB_MAX_TRIP_DISTANCE=2680
 NB_BOUNDARY_BUFFER=$NB_MAX_TRIP_DISTANCE
 
@@ -16,10 +15,9 @@ function import_and_transform_shapefile() {
   echo "START: Importing ${IMPORT_TABLENAME}"
   # https://manpages.debian.org/stretch/postgis/shp2pgsql.1.en.html
   shp2pgsql -c -I -D -s "${IMPORT_SRID}" "${IMPORT_FILE}" "${IMPORT_TABLENAME}" | psql
-  # shp2pgsql -I -d -D -s "${IMPORT_SRID}" "${IMPORT_FILE}" "${IMPORT_TABLENAME}" | psql
+  shp2pgsql -d -I -D -s "${IMPORT_SRID}" "${IMPORT_FILE}" "${IMPORT_TABLENAME}" | psql
   psql -c "ALTER TABLE ${IMPORT_TABLENAME} ALTER COLUMN geom \
             TYPE geometry(MultiPolygon,${NB_OUTPUT_SRID}) USING ST_Force2d(ST_Transform(geom,${NB_OUTPUT_SRID}));"
-  # remyg: do we need the alter command if we specify the output SRID in the shp2pgsql command? -s"${IMPORT_SRID}:${NB_OUTPUT_SRID}"
   echo "DONE: Importing ${IMPORT_TABLENAME}"
 }
 
@@ -27,6 +25,16 @@ function import_and_transform_shapefile() {
 import_and_transform_shapefile "${NB_BOUNDARY_FILE}" neighborhood_boundary "${NB_INPUT_SRID}"
 
 NB_COUNTRY=$(echo "$NB_COUNTRY" | tr '[:lower:]' '[:upper:]')
+
+# Import neighborhood boundary.
+echo "IMPORTING: Loading neighborhood boundary: ${NB_BOUNDARY_FILE}"
+import_and_transform_shapefile "${NB_BOUNDARY_FILE}" neighborhood_boundary "${NB_INPUT_SRID}"
+psql <<SQL
+ALTER TABLE neighborhood_boundary ALTER COLUMN geom
+TYPE GEOMETRY (MULTIPOLYGON, ${NB_OUTPUT_SRID}) USING ST_FORCE2D(ST_TRANSFORM(geom, ${NB_OUTPUT_SRID}));
+SQL
+
+# Import waterblocks.
 if [ "${NB_COUNTRY}" == "USA" ]; then
   echo "IMPORTING: Downloading water blocks"
   psql <"${GIT_ROOT}/sql/create_us_water_blocks_table.sql"
@@ -36,7 +44,11 @@ fi
 
 # Import block shapefile
 echo "IMPORTING: Loading census blocks"
-import_and_transform_shapefile "${NB_TEMPDIR}/population.shp" neighborhood_census_blocks 4326
+import_and_transform_shapefile "${NB_TEMPDIR}/population.shp" neighborhood_census_blocks "${NB_INPUT_SRID}"
+psql <<SQL
+ALTER TABLE neighborhood_census_blocks ALTER COLUMN geom
+TYPE GEOMETRY (MULTIPOLYGON, ${NB_OUTPUT_SRID}) USING ST_FORCE2D(ST_TRANSFORM(geom, ${NB_OUTPUT_SRID}));
+SQL
 
 # Only keep blocks in boundary+buffer
 echo "IMPORTING: Applying boundary buffer"
@@ -57,7 +69,7 @@ fi
 
 POPULATION=$(psql -t -c "SELECT SUM(pop10) FROM neighborhood_census_blocks;")
 echo "Population is ${POPULATION}"
-if [[ $POPULATION -eq 0 ]]; then
+if [ "$POPULATION" -eq 0 ]; then
   echo "Error: Total population of included census blocks is zero."
   exit 1
 fi
